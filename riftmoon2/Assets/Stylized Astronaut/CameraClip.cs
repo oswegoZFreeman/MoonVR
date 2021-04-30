b1137242b7107bc35f7d26c4d071b73924fa6bc7
ZachF <zfreeman@oswego.edu>
2021-04-30 10:15:10 -0400

    riftmoon2

---
+++ riftmoon2/Assets/Stylized Astronaut/CameraClip.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraClip : MonoBehaviour
+{
+     RaycastHit ray;
+ Player player;
+ LayerMask worldLayerMask;
+ public float clipOffset = 0.1f;
+ public Vector3 clipCheckOffset = new Vector3(0, 1, 0);
+ void Update()
+ {
+
+     Debug.DrawRay(GetComponent<Camera>().transform.position, (
+   (player.transform.position + clipCheckOffset) -
+     GetComponent<Camera>().transform.position));
+
+     if (
+     Physics.Raycast(GetComponent<Camera>().transform.position, (player.transform.position + clipCheckOffset) -
+     GetComponent<Camera>().transform.position, out ray, -GetComponent<Camera>().transform.localPosition.z - 0.5f,
+     worldLayerMask)
+     )
+     {
+         GetComponent<Camera>().transform.position = ray.point + ((player.transform.position + clipCheckOffset) -
+         GetComponent<Camera>().transform.position).normalized * clipOffset;
+         Debug.Log("hit something");
+     }
+
+
+
+ }
+}
---
+++ riftmoon2/Assets/Stylized Astronaut/Jump.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class Jump : MonoBehaviour {
+    Rigidbody rb;
+	private float jumpForce = 1000f;
+	private bool onGround;
+	
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+		onGround = true;
+    }
+    void Update()
+    {
+        if (Input.GetButton("Jump") && onGround == true)
+        {
+			
+            rb.velocity = new Vector3( 0f, jumpForce, 0f);
+			onGround = false;
+        }
+    }
+	void OnCollisionEnter(Collision other) {
+		if(other.gameObject.CompareTag("ground")){
+			onGround = true;
+		}
+	}
+
+}
